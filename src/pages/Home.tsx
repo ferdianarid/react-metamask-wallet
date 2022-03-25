52cbd106a1704da68d7400866d536f8b73039b02
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-03-25 17:15:29 +0700

    initial commit finish setup ✨

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <React.Fragment>
+            <button className="btn btn-primary">Metamask Wallet</button>
+        </React.Fragment>
+    )
+}
+
+export default Home
